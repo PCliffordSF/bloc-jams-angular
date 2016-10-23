2fde91bbc93bb05a083d3999c1025508c47cfce7
Paul Clifford <paulclifford2570@gmail.com>
2016-10-23 13:27:23 +0000

    up to Write a pause Method

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,17 +2,34 @@
      function SongPlayer() { // constuctor fucntion
           var SongPlayer = {};
           
-         SongPlayer.play = function(song) {
-         var currentBuzzObject = new buzz.sound(song.audioUrl, {
-             formats: ['mp3'],
-             preload: true
-         });
- 
-         currentBuzzObject.play();    
-     };
+            var currentSong = null;
+            var currentBuzzObject = null;
           
-          return SongPlayer; // returned objecct to be injected. 
-     }
+         SongPlayer.play = function(song) {
+             console.log(song);
+             console.log(currentSong);
+            if (currentSong !== song) {
+                if (currentBuzzObject) {
+                    currentBuzzObject.stop(); // stop current song if new song is played
+                    currentSong.playing = null; // this is used in ng-show in albub.html
+                } else if (currentSong === song) {
+                    if (currentBuzzObject.isPaused()) {
+                    currentBuzzObject.play(); // play current song if current song is paused.
+                }
+             } 
+             
+             currentBuzzObject = new buzz.sound(song.audioUrl, {
+                formats: ['mp3'],
+                preload: true
+            });
+         
+                currentSong = song;
+                currentBuzzObject.play();
+                song.playing = true; // this is used in ng-show in albub.html
+            };
+        }
+        return SongPlayer; // returned objecct to be injected.
+    }
  
      angular
          .module('blocJams')
